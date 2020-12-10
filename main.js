6f2023b0f2ad77b2be227f5e58524b4366d60434
Rob W <robpwatkins@gmail.com>
2020-12-10 12:29:54 -0600

    getting there

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-const cars = ['Ford', 'Toyota', 'BMW', 'Jeep'];
+// const cars = ['Ford', 'Toyota', 'BMW', 'Jeep'];
+// console.log(cars.length);
+
+// const moreCars = ['Nissan', 'Acura', 'Infinity', 'Honda'];
+// console.log(moreCars);
+
+// let totalCars = cars.concat(moreCars);
+// console.log(totalCars);
+// console.log(totalCars.indexOf('Honda'));
+// console.log(totalCars.lastIndexOf('Ford'));
+
+// let stringOfCars = totalCars.join(' ');
+// console.log(stringOfCars);
+
+// let totalCarsFromString = stringOfCars.split(' ');
+// console.log(totalCarsFromString);
+
+// let carsInReverse = totalCars.reverse();
+// console.log(carsInReverse);
+
+// carsInReverse.sort();
+// console.log(carsInReverse.indexOf('Acura') == 0);
+
+const cars = ['Ford', 'Chevy', 'Lincoln', 'Toyota'];
 console.log(cars.length);
 
-const moreCars = ['Nissan', 'Acura', 'Infinity', 'Honda'];
-console.log(moreCars);
-
-let totalCars = cars.concat(moreCars);
+const moreCars = ['BMW', 'Nissan', 'Mitsubishi', 'Honda'];
+const totalCars = cars.concat(moreCars);
 console.log(totalCars);
 console.log(totalCars.indexOf('Honda'));
 console.log(totalCars.lastIndexOf('Ford'));
 
-let stringOfCars = totalCars.join(' ');
+const stringOfCars = totalCars.join(' ');
 console.log(stringOfCars);
 
-let totalCarsFromString = stringOfCars.split(' ');
-console.log(totalCarsFromString);
-
-let carsInReverse = totalCars.reverse();
-console.log(carsInReverse);
-
-carsInReverse.sort();
-console.log(carsInReverse.indexOf('Acura'));
+const carsFromString = stringOfCars.split(' ');
+console.log(carsFromString);
